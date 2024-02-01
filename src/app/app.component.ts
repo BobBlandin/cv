47a0fd6072d614985a578ba3b88d10b1c8b6ed9e
Bob <bob.blandin@gmail.com>
2024-02-01 21:50:31 +0100

    feat: add pdf generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,70 @@
-import {DatePipe, NgForOf, NgIf} from "@angular/common";
-import {Component, OnInit} from '@angular/core';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import PersonalData from "../assets/bob_blandin.json";
-import {ExperienceComponent} from "./components/experience/experience.component";
-import {HeaderComponent} from "./components/header/header.component";
-import {InformationsComponent} from "./components/informations/informations.component";
-import {Data} from "./model/data.model";
+import jsPDF from 'jspdf';
+import PersonalData from '../assets/bob_blandin.json';
+
+import {ExperienceComponent} from './components/experience/experience.component';
+import {HeaderComponent} from './components/header/header.component';
+import {InformationsComponent} from './components/informations/informations.component';
+import {calibri_bold} from "./jspdf-fonts/calibri-bold";
+import {calibri_normal} from "./jspdf-fonts/calibri-normal";
+import {Data} from './model/data.model';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NgIf, DatePipe, InformationsComponent, HeaderComponent, ExperienceComponent, NgForOf],
+    imports: [
+        RouterOutlet,
+        NgIf,
+        DatePipe,
+        InformationsComponent,
+        HeaderComponent,
+        ExperienceComponent,
+        NgForOf,
+    ],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-    data: Data | undefined = undefined;
+    data: Data = undefined;
 
+    @ViewChild('cvRoot', {static: false}) public cvRoot: ElementRef;
 
     ngOnInit(): void {
         this.data = PersonalData as unknown as Data;
+    }
+
+    downloadAsPdf(): void {
+        const rootElement = this.cvRoot.nativeElement;
+        const width = rootElement.getBoundingClientRect().width;
+        const height = rootElement.getBoundingClientRect().height;
+
+
+        // Add 0.5 to height to avoid a new blank page
+        const pdf = new jsPDF('p', 'px', [width, height + 0.5]);
+        pdf.addFileToVFS('calibri.ttf', calibri_normal);
+        pdf.addFont('calibri.ttf', 'calibri', 'normal');
+        pdf.addFileToVFS('calibri.ttf', calibri_bold);
+        pdf.addFont('calibri.ttf', 'calibri', 'bold');
+
+        pdf.setFont('calibri', 'normal');
+        //
+        pdf.html(rootElement, {
+            callback: (pdf) => {
+                rootElement.querySelectorAll("a").forEach((a) => {
+                    pdf.link(a.offsetLeft, a.offsetTop, a.offsetWidth, a.offsetHeight, {url: a.href});
+                });
+
+                window.open(pdf.output('bloburl'), '_blank');
+                // pdf.save(
+                //     'CV_' +
+                //     this.data.personal.firstName.toUpperCase() +
+                //     '_' +
+                //     this.data.personal.lastName.toUpperCase() +
+                //     '.pdf',
+                // );
+            },
+        });
     }
 }
